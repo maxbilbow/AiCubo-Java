60c1d22057221210f0d25ac742411813e4d2833a
maxbilbow <maxbilbow@gmail.com>
2015-08-16 23:43:46 +0100

    fixed

---
+++ AiCubo/src/rmx/engine/PhysicsBody.java
@@ -0,0 +1,22 @@
+package rmx.engine;
+
+
+public class PhysicsBody extends NodeComponent {
+
+	float mass = 0;
+	public float getMass() {
+		return mass;
+	}
+	
+	public float TotalMass() {
+		return this.getNode().transform.mass();
+	}
+	
+	
+	
+	public void setMass(float mass) {
+		this.mass = mass;
+	}
+
+	
+}
---
+++ AiCubo/src/rmx/engine/math/Vector3.java
@@ -0,0 +1,19 @@
+package rmx.engine.math;
+
+import javax.vecmath.Vector3f;
+
+public class Vector3 extends Vector3f {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -830815158587681541L;
+
+	public Vector3() {
+		super();
+	}
+	public Vector3(float x, float y, float z) {
+		super(x,y,z);
+	}
+
+	static final int SIZE = 3;
+}
